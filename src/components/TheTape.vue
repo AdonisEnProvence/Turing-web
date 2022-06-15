691948a1a7b4d80fd9f54e49913fac90729a1fcc
Devessier <baptiste@devessier.fr>
2022-06-15 19:04:36 +0200

    feat: combine empty squares and squares so that all of them animate the same

--- src/components/TheTape.vue
+++ src/components/TheTape.vue
@@ -1,108 +1,62 @@
 <script lang="ts" setup>
 import { ref, computed } from "vue";
 
-const steps: [tape: (number | undefined)[], indexOfHead: number][] = [
-  [[1, 0, 1], 0],
-  [[1, 0, 1], 1],
-  [[1, 0, 1], 2],
-  [[1, 0, 1, 0], 3],
-  [[1, 0, 1, 0, 1], 4],
-  [[1, 0, 1, 0, 1], 3],
-  [[1, 0, 1, 0, 1], 4],
-  [[1, 0, 1, 0, 1, 1], 5],
-  [[1, 0, 1, 0, 1, 1, 1], 6],
-  [[1, 0, 1, 0, 1, 1, 1, 1], 7],
-  [[1, 0, 1, 0, 1, 1, 1, 1, 0], 8],
-  [[1, 0, 1, 0, 1, 1, 1, 1, 0, 1], 9],
-  [[1, 0, 1, 0, 1, 1, 1, 1, 0, 1, 0], 10],
-  [[1, 0, 1, 0, 1, 1, 1, 1, 0, 1, 0], 9],
-  [[1, 0, 1, 0, 1, 1, 1, 1, 0, 1, 0], 8],
-  [[1, 0, 1, 0, 1, 1, 1, 1, 0, 1, 0], 7],
-  [[1, 0, 1, 0, 1, 1, 1, 1, 0, 1, 0], 6],
-  [[1, 0, 1, 0, 1, 1, 1, 1, 0, 1, 0], 5],
-  [[1, 0, 1, 0, 1, 1, 1, 1, 0, 1, 0], 4],
-  [[1, 0, 1, 0, 1, 1, 1, 1, 0, 1, 0], 3],
-  [[1, 0, 1, 0, 1, 1, 1, 1, 0, 1, 0], 2],
-  [[1, 0, 1, 0, 1, 1, 1, 1, 0, 1, 0], 1],
-  [[1, 0, 1, 0, 1, 1, 1, 1, 0, 1, 0], 0],
-  [[undefined, 1, 0, 1, 0, 1, 1, 1, 1, 0, 1, 0], 0],
-  [[undefined, undefined, 1, 0, 1, 0, 1, 1, 1, 1, 0, 1, 0], 0],
-  [[undefined, undefined, undefined, 1, 0, 1, 0, 1, 1, 1, 1, 0, 1, 0], 0],
-  [
+// Must be an odd number.
+const displayedTapeLength = 13;
+const squaresAmountOnOneSideOfHead = Math.floor(displayedTapeLength / 2);
+const addedBlankSpace = squaresAmountOnOneSideOfHead;
+
+type Steps = [tape: string[], indexOfHead: number][];
+
+const blankCharacter = ".";
+const steps: Steps = fillStepsWithBlankSpacesOnSides([
+  [["0", "0", "1", "1"], 0],
+  [[".", "0", "1", "1"], 1],
+  [[".", "0", "1", "1"], 2],
+  [[".", "0", "1", "1"], 3],
+  [[".", "0", "1", "1", "."], 4],
+  [[".", "0", "1", "1", "."], 3],
+  [[".", "0", "1", ".", "."], 2],
+  [[".", "0", "1", ".", "."], 1],
+  [[".", "0", "1", ".", "."], 0],
+  [[".", "0", "1", ".", "."], 1],
+  [[".", ".", "1", ".", "."], 2],
+  [[".", ".", "1", ".", "."], 3],
+  [[".", ".", "1", ".", "."], 2],
+  [[".", ".", ".", ".", "."], 1],
+  [[".", ".", ".", ".", "."], 2],
+  [[".", ".", "y", ".", "."], 1],
+]);
+
+/**
+ * We add enough blank squares to all steps so that
+ * there will always be the same amount of squares on the tape.
+ */
+function fillStepsWithBlankSpacesOnSides(steps: Steps): Steps {
+  return steps.map(([tape, indexOfHead]) => [
     [
-      undefined,
-      undefined,
-      undefined,
-      undefined,
-      1,
-      0,
-      1,
-      0,
-      1,
-      1,
-      1,
-      1,
-      0,
-      1,
-      0,
+      ...Array.from({ length: addedBlankSpace }).map(() => blankCharacter),
+      ...tape,
+      ...Array.from({ length: addedBlankSpace }).map(() => blankCharacter),
     ],
-    0,
-  ],
-  [
-    [
-      undefined,
-      undefined,
-      undefined,
-      undefined,
-      undefined,
-      1,
-      0,
-      1,
-      0,
-      1,
-      1,
-      1,
-      1,
-      0,
-      1,
-      0,
-    ],
-    0,
-  ],
-  [
-    [
-      undefined,
-      undefined,
-      undefined,
-      undefined,
-      undefined,
-      undefined,
-      1,
-      0,
-      1,
-      0,
-      1,
-      1,
-      1,
-      1,
-      0,
-      1,
-      0,
-    ],
-    0,
-  ],
-];
+    indexOfHead,
+  ]);
+}
 
 const keysReference = steps[steps.length - 1][0].map((_, index) => index);
 
-type StepWithKey = [
-  tape: { key: string; value: number | undefined }[],
-  indexOfHead: number
-];
+type TapeSquareWithKey = { key: string; value: string };
 
-const tapeListWithKey: StepWithKey[] = steps
-  .reverse()
-  .map(([tape, indexOfHead]) => {
+type StepWithKey = [tape: TapeSquareWithKey[], indexOfHead: number];
+
+function computeTapeListWithFixedKeys() {
+  const tapeListWithKeyAndBlanks: StepWithKey[] = [];
+
+  /**
+   * Start from the biggest tape to ensure
+   * that keys remain the same for smaller tapes.
+   */
+  for (const [tape, indexOfHead] of steps.slice().reverse()) {
     if (tape.length !== keysReference.length) {
       const tapeIsReducedOnLeft = indexOfHead === 0;
 
@@ -113,46 +67,37 @@ const tapeListWithKey: StepWithKey[] = steps
       }
     }
 
-    return [
-      tape.map((value, index) => ({
-        key: `square:${keysReference[index]}`,
-        value,
-      })),
+    tapeListWithKeyAndBlanks.push([
+      tape.map(
+        (value, index) =>
+          ({
+            key: `square:${keysReference[index]}`,
+            value,
+          } as TapeSquareWithKey)
+      ),
       indexOfHead,
-    ] as StepWithKey;
-  })
-  .reverse();
+    ]);
+  }
+
+  tapeListWithKeyAndBlanks.reverse();
+
+  return tapeListWithKeyAndBlanks;
+}
 
 const indexOnStepList = ref(0);
 
-const tape = computed(() => tapeListWithKey[indexOnStepList.value][0]);
+const tapeListWithKeyAndBlanks = computeTapeListWithFixedKeys();
+
+const tape = computed(() => tapeListWithKeyAndBlanks[indexOnStepList.value][0]);
 const headIndexOnTape = computed(
-  () => tapeListWithKey[indexOnStepList.value][1]
+  () => tapeListWithKeyAndBlanks[indexOnStepList.value][1]
 );
 
-const displayedTapeLength = 11;
-const emptyTape = Array.from({ length: displayedTapeLength }, (_, index) => ({
-  key: `blank:${index}`,
-  value: undefined,
-}));
-
 const displayedTape = computed(() => {
-  const inferiorBoundToDisplay = Math.max(0, headIndexOnTape.value - 5);
-  const superiorBoundToDisplay = Math.min(
-    tape.value.length,
-    headIndexOnTape.value + 6
+  return tape.value.slice(
+    headIndexOnTape.value,
+    displayedTapeLength + headIndexOnTape.value
   );
-  const squaresRemaingOnRightOfTape = tape.value.length - headIndexOnTape.value;
-
-  return [
-    ...(headIndexOnTape.value < 6
-      ? emptyTape.slice(0, 5 - headIndexOnTape.value)
-      : []),
-    ...tape.value.slice(inferiorBoundToDisplay, superiorBoundToDisplay),
-    ...(squaresRemaingOnRightOfTape < 6
-      ? emptyTape.slice(5 + squaresRemaingOnRightOfTape)
-      : []),
-  ];
 });
 
 function goToPreviousIteration() {
@@ -174,22 +119,37 @@ function goToNextIteration() {
     <p>Step: {{ indexOnStepList }}</p>
 
     <div>
-      <div>
-        <div class="relative inline-flex">
+      <div
+        class="overflow-hidden"
+        :style="{
+          width: `calc(${displayedTapeLength} * 2.5rem - (2.5rem / 6 * 5 * 2))`,
+        }"
+      >
+        <div
+          class="relative inline-flex"
+          :style="{ marginLeft: 'calc(-1 * 2.5rem / 6 * 5)' }"
+        >
           <TransitionGroup
-            enter-active-class="transition-all duration-200"
-            move-class="transition-all duration-200"
-            leave-active-class="absolute transition-all duration-200"
-            enter-from-class="opacity-0"
-            leave-from-class="opacity-50"
+            leave-from-class="opacity-0"
+            leave-active-class="absolute transition-all duration-[0s]"
             leave-to-class="opacity-0"
+            move-class="transition-all duration-200"
           >
             <div
               v-for="{ key, value } in displayedTape"
+              :id="key"
               :key="key"
               class="flex items-center justify-center w-10 h-10 text-center border border-gray-100"
             >
-              <p>{{ value }}</p>
+              <Transition
+                mode="out-in"
+                enter-from-class="opacity-0"
+                enter-active-class="transition-opacity duration-100"
+                leave-to-class="opacity-0"
+                leave-active-class="transition-opacity duration-100"
+              >
+                <p :key="value">{{ value }}</p>
+              </Transition>
             </div>
           </TransitionGroup>
 
